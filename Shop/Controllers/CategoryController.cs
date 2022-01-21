fffc2751f4245af55db55c44159f0c55afa6d14d
Amin Saffarnejad <vira1368@gmail.com>
2022-01-21 14:40:34 +0330

    add category controller.

---
+++ Shop/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
